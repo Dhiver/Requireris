95d73ce88a60a7f652016bad1825b90398807a90
Bastien DHIVER <epitech@bastn.fr>
2016-11-01 17:26:26 +0100

    Add comments in hotp.js

--- algos/hotp.js
+++ algos/hotp.js
@@ -1,32 +1,36 @@
 const crypto = require('crypto');
+const assert = require('assert');
 const base32 = require('base32');
 
 function hmac(hash, secret, msg) {
-	return crypto.createHmac(hash, secret).update(msg).digest()
+	return crypto.createHmac(hash, secret).update(msg).digest();
 }
 
 function getBufferSubset(buf, offset, length) {
-	return new Buffer(buf).slice(offset, offset + length)
+	return new Buffer(buf).slice(offset, offset + length);
 }
 
 function dynamicTruncation(hs) {
 	// Get offset value from the hs last byte low-order nibble
-	const offset = hs[hs.length - 1] & 0xf
+	const offset = hs[hs.length - 1] & 0xf;
 	// Only get 4-byte from hs at offset
-	const dbc1 = getBufferSubset(hs, offset, 4)
-	dbc2 = new Buffer(dbc1)
+	const dbc1 = getBufferSubset(hs, offset, 4);
+	dbc2 = new Buffer(dbc1);
 	// Masking the most significant bit of dbc2
-	dbc2[0] &= 0x7f
-	return dbc2.readUIntBE(0, dbc2.length)
+	dbc2[0] &= 0x7f;
+	return dbc2.readUIntBE(0, dbc2.length);
 }
 
 function hotp(secret, counter, len, hash) {
-	const decodedSecret = base32.decode(secret)
-	const hs = hmac(hash, decodedSecret, counter)
-	const sbits = dynamicTruncation(hs).toString()
-	return sbits.substr(sbits.length - len)
+	len = len < 6 ? 6 : len;
+	len = len > 8 ? 8 : len;
+	assert(secret.length >= 16, "shared secret must be at least 16 bytes")
+	const decodedSecret = base32.decode(secret);
+	const hs = hmac(hash, decodedSecret, counter);
+	const sbits = dynamicTruncation(hs).toString();
+	return sbits.substr(sbits.length - len);
 }
 
-const date = (new Date).getTime().toString()
+const date = (new Date).getTime().toString();
 
-console.log(hotp('JBSWY3DPEHPK3PXP', date, 6, 'sha1'))
+console.log(hotp('JBSWY3DPEHPK3PXP', date, 6, 'sha1'));
